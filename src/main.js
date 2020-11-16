19b95b576c3672873aee3eece7e55946a315248f
dineshaswin <aswin@billionlives.in>
2020-11-16 10:49:16 +0530

    -update 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
 new Vue({
   el: '#app',
   render: h => h(App)
